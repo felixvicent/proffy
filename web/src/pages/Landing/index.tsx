15519fd4ff7eb503940b9fa3f8187dd19f5fe9a7
Félix Vicente <felixvicent1306@gmail.com>
2020-08-05 16:45:18 -0300

    Terceiro dia

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -10,6 +12,14 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      setTotalConnections(response.data.total);
+    })
+  }, [])
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -29,7 +39,7 @@ function Landing() {
           </Link>
         </div>
         <span className="total-connections">
-          Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
+          Total de { totalConnections } conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
     </div>
